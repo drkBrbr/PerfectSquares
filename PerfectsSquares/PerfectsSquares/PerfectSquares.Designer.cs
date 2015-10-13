60d5a4d2e27fe0df1df1d9110db64f203602b3b4
drkBrbr <derek.barber@ocsbstudent.ca>
2015-10-13 13:30:38 -0400

    First commit

---
+++ PerfectsSquares/PerfectsSquares/PerfectSquares.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PerfectsSquares
+{
+    partial class frmPerfectSquares
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lstPerfectSquares = new System.Windows.Forms.ListBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(270, 226);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lstPerfectSquares
+            // 
+            this.lstPerfectSquares.FormattingEnabled = true;
+            this.lstPerfectSquares.Location = new System.Drawing.Point(12, 77);
+            this.lstPerfectSquares.Name = "lstPerfectSquares";
+            this.lstPerfectSquares.Size = new System.Drawing.Size(120, 173);
+            this.lstPerfectSquares.TabIndex = 1;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(12, 13);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(345, 13);
+            this.lblDescription.TabIndex = 2;
+            this.lblDescription.Text = "Pick a number to see all of the perfect squares from zero to your number";
+            // 
+            // nudNumber
+            // 
+            this.nudNumber.Location = new System.Drawing.Point(12, 51);
+            this.nudNumber.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(120, 20);
+            this.nudNumber.TabIndex = 3;
+            // 
+            // frmPerfectSquares
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 261);
+            this.Controls.Add(this.nudNumber);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lstPerfectSquares);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "frmPerfectSquares";
+            this.Text = "Perfect Squares";
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.ListBox lstPerfectSquares;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.NumericUpDown nudNumber;
+    }
+}
+
---
+++ PerfectsSquares/PerfectsSquares/PerfectSquares.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectsSquares
+{
+    public partial class frmPerfectSquares : Form
+    {
+        public frmPerfectSquares()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            int valueCounter;
+            int maximumValue;
+            double currentValue;
+            double squareRootAsDouble;
+            int squareRootAsInteger;
+            int numberOfPerfectSquares = 0;
+
+            this.lstPerfectSquares.Items.Clear();
+            currentValue = 1;
+            maximumValue = Convert.ToInt32(this.nudNumber.Value);
+            valueCounter = 0;
+
+            while (currentValue <= maximumValue)
+            {
+                squareRootAsDouble = Math.Sqrt(currentValue);
+                squareRootAsInteger = Convert.ToInt32(squareRootAsDouble);
+                if (squareRootAsInteger == squareRootAsDouble)
+                {
+                    numberOfPerfectSquares++;
+                    this.lstPerfectSquares.Items.Add(currentValue);
+                    currentValue++;
+                }
+                else
+                {
+                    currentValue++;
+                }
+                valueCounter++;
+            }
+
+
+
+        }
+    }
+}
